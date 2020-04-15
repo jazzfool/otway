ef04e0161ee9462b23c2b0e3a405206eddb1d3ae
jazzfool <saveuselon@gmail.com>
2020-04-15 14:41:40 +1000

    refine theming interface

--- src/theme/flat.rs
+++ src/theme/flat.rs
@@ -1,45 +1,138 @@
 use {
-    crate::{kit, theme::*, ui},
+    crate::{kit, prelude::*, theme::*, ui},
     reclutch::display as gfx,
 };
 
-pub struct FlatTheme {}
+const CORNER_RADIUS: f32 = 5.0;
+const CORNER_RADII: [f32; 4] = [CORNER_RADIUS, CORNER_RADIUS, CORNER_RADIUS, CORNER_RADIUS];
+
+pub type FontRef = (gfx::ResourceReference, gfx::FontInfo);
+
+#[derive(Clone)]
+pub struct Fonts {
+    ui_regular: FontRef,
+}
+
+#[derive(Clone, Copy)]
+pub struct FontSizes {
+    ui: f32,
+}
+
+pub struct FlatTheme {
+    fonts: Fonts,
+    font_sizes: FontSizes,
+}
 
 impl FlatTheme {
-    pub fn new() -> Self {
-        FlatTheme {}
+    pub fn new(
+        display: &mut dyn gfx::GraphicsDisplay,
+        fonts: Option<Fonts>,
+        font_sizes: Option<FontSizes>,
+    ) -> Result<Self, ThemeError> {
+        let fonts = if let Some(fonts) = fonts {
+            fonts
+        } else {
+            Fonts {
+                ui_regular: {
+                    let info = gfx::FontInfo::from_name(
+                        "Segoe UI", // Windows
+                        &[
+                            "SF Display",      // MacOS
+                            "Helvetica",       // MacOS
+                            "Lucida Grande",   // MacOS
+                            "Noto Sans",       // Linux
+                            "Liberation Sans", // Linux
+                            "Cantarell",       // Linux
+                        ],
+                        None,
+                    )?;
+
+                    let reference = display.new_resource(gfx::ResourceDescriptor::Font(
+                        gfx::ResourceData::Data(gfx::SharedData::RefCount(std::sync::Arc::new(
+                            info.data().ok_or(ThemeError::ResourceError(
+                                reclutch::error::ResourceError::InvalidData,
+                            ))?,
+                        ))),
+                    ))?;
+
+                    (reference, info)
+                },
+            }
+        };
+
+        let font_sizes = font_sizes.unwrap_or_else(|| FontSizes { ui: 12.0 });
+
+        Ok(FlatTheme { fonts, font_sizes })
     }
 }
 
 impl<T: 'static> Theme<T> for FlatTheme {
     fn painter(&self, p: &'static str) -> Box<dyn AnyPainter<T>> {
         match p {
-            painters::BUTTON => Box::new(ButtonPainter),
+            painters::BUTTON => Box::new(ButtonPainter {
+                fonts: self.fonts.clone(),
+                font_sizes: self.font_sizes,
+            }),
             _ => unimplemented!(),
         }
     }
 
     fn color(&self, c: &'static str) -> gfx::Color {
         match c {
+            colors::FOREGROUND => gfx::Color::new(0.8, 0.8, 0.8, 0.8),
+            colors::BACKGROUND => gfx::Color::new(0.0, 0.0, 0.0, 1.0),
+            colors::WEAK_FOREGROUND => gfx::Color::new(0.5, 0.5, 0.5, 1.0),
+            colors::STRONG_FOREGROUND => gfx::Color::new(1.0, 1.0, 1.0, 1.0),
             _ => unimplemented!(),
         }
     }
 }
 
-struct ButtonPainter;
+struct ButtonPainter {
+    fonts: Fonts,
+    font_sizes: FontSizes,
+}
+
+impl ButtonPainter {
+    fn text_bounds(&self, text: gfx::DisplayText) -> gfx::Size {
+        gfx::TextDisplayItem {
+            text,
+            bottom_left: Default::default(),
+            color: gfx::StyleColor::Color(Default::default()),
+            font: self.fonts.ui_regular.0,
+            font_info: self.fonts.ui_regular.1.clone(),
+            size: self.font_sizes.ui,
+        }
+        .bounds()
+        .unwrap()
+        .size
+    }
+}
 
 impl<T: 'static> TypedPainter<T> for ButtonPainter {
     type Object = kit::Button<T>;
 
     fn paint(
         &mut self,
-        _obj: &mut kit::Button<T>,
+        obj: &mut kit::Button<T>,
         aux: &mut ui::Aux<T>,
     ) -> Vec<gfx::DisplayCommand> {
-        let out = gfx::DisplayListBuilder::new();
+        let mut out = gfx::DisplayListBuilder::new();
 
-        aux.theme.color("foreground");
+        out.push_round_rectangle(
+            obj.common().get_ref().rect(),
+            CORNER_RADII,
+            gfx::GraphicsDisplayPaint::Fill(gfx::StyleColor::Color(
+                aux.theme.color(colors::FOREGROUND),
+            )),
+            None,
+        );
 
         out.build()
+    }
+
+    #[inline]
+    fn size_hint(&mut self, obj: &mut kit::Button<T>) -> gfx::Size {
+        self.text_bounds(obj.text().clone())
     }
 }
--- src/theme/mod.rs
+++ src/theme/mod.rs
@@ -8,7 +8,15 @@
 #[cfg(feature = "themes")]
 pub mod flat;
 
-use {crate::ui, reclutch::display as gfx};
+use {crate::ui, reclutch::display as gfx, thiserror::Error};
+
+#[derive(Debug, Error)]
+pub enum ThemeError {
+    #[error("failed to load theme resource: {0}")]
+    ResourceError(#[from] reclutch::error::ResourceError),
+    #[error("failed to load theme font: {0}")]
+    FontError(#[from] reclutch::error::FontError),
+}
 
 pub struct Painter<O: 'static, T: 'static>(
     Option<Box<dyn AnyPainter<T>>>,
@@ -19,6 +27,7 @@ pub trait TypedPainter<T: 'static>: AnyPainter<T> {
     type Object: 'static;
 
     fn paint(&mut self, obj: &mut Self::Object, aux: &mut ui::Aux<T>) -> Vec<gfx::DisplayCommand>;
+    fn size_hint(&mut self, obj: &mut Self::Object) -> gfx::Size;
 }
 
 pub trait AnyPainter<T: 'static> {
@@ -27,15 +36,22 @@ pub trait AnyPainter<T: 'static> {
         obj: &mut dyn std::any::Any,
         aux: &mut ui::Aux<T>,
     ) -> Vec<gfx::DisplayCommand>;
+    fn size_hint(&mut self, obj: &mut dyn std::any::Any) -> gfx::Size;
 }
 
 impl<T: 'static, P: TypedPainter<T>> AnyPainter<T> for P {
+    #[inline]
     fn paint(
         &mut self,
         obj: &mut dyn std::any::Any,
         aux: &mut ui::Aux<T>,
     ) -> Vec<gfx::DisplayCommand> {
         TypedPainter::paint(self, obj.downcast_mut::<P::Object>().unwrap(), aux)
+    }
+
+    #[inline]
+    fn size_hint(&mut self, obj: &mut dyn std::any::Any) -> gfx::Size {
+        TypedPainter::size_hint(self, obj.downcast_mut::<P::Object>().unwrap())
     }
 }
 
@@ -55,6 +71,16 @@ pub fn paint<O: 'static, T: 'static>(
 ) -> Vec<gfx::DisplayCommand> {
     let mut painter = p(obj).0.take().unwrap();
     let out = AnyPainter::paint(&mut *painter, obj, aux);
+    p(obj).0 = Some(painter);
+    out
+}
+
+pub fn size_hint<O: 'static, T: 'static>(
+    obj: &mut O,
+    p: impl Fn(&mut O) -> &mut Painter<O, T>,
+) -> gfx::Size {
+    let mut painter = p(obj).0.take().unwrap();
+    let out = AnyPainter::size_hint(&mut *painter, obj);
     p(obj).0 = Some(painter);
     out
 }
