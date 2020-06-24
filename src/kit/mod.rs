5fbe1271c04fd4a6f3a5ad705a8a1e3f62326359
jazzfool <saveuselon@gmail.com>
2020-06-24 15:10:06 +1000

    add text box widget

--- src/kit/mod.rs
+++ src/kit/mod.rs
@@ -5,18 +5,34 @@ use {
 
 pub mod button;
 pub mod label;
+pub mod text_box;
 
-pub use {button::*, label::*};
+pub use {button::*, label::*, text_box::*};
 
 /// The widget was pressed.
+#[repr(transparent)]
 pub struct PressEvent(pub gfx::Point);
 /// The widget was released from its press ([`PressEvent`](PressEvent)).
+#[repr(transparent)]
 pub struct ReleaseEvent(pub gfx::Point);
 /// The cursor entered the widget boundaries.
+#[repr(transparent)]
 pub struct BeginHoverEvent(pub gfx::Point);
 /// The cursor left the widget boundaries.
+#[repr(transparent)]
 pub struct EndHoverEvent(pub gfx::Point);
 
+pub struct FocusGainedEvent;
+pub struct FocusLostEvent;
+
+#[repr(transparent)]
+pub struct KeyPressEvent(pub ui::KeyInput);
+#[repr(transparent)]
+pub struct KeyReleaseEvent(pub ui::KeyInput);
+#[repr(transparent)]
+pub struct TextEvent(pub char);
+
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum InteractionEvent {
     Press(gfx::Point),
     Release(gfx::Point),
@@ -28,8 +44,10 @@ pub fn interaction_handler<T, W: ui::WidgetChildren<T>>(
     aux: &mut ui::Aux<T>,
     callback: impl Fn(&mut W, &mut ui::Aux<T>, InteractionEvent) + Copy + 'static,
     mask: impl Into<Option<InteractionMask>>,
+    ignore_visibility: impl Into<Option<bool>>,
 ) -> ui::Listener<W, ui::Aux<T>> {
     let mask = mask.into().unwrap_or(Default::default());
+    let ignore_vis = ignore_visibility.into().unwrap_or(false);
     aux.listen()
         .and_on(
             aux.id,
@@ -37,6 +55,11 @@ pub fn interaction_handler<T, W: ui::WidgetChildren<T>>(
                 if !mask.press {
                     return;
                 }
+                let v = obj.visible();
+                if !ignore_vis && invisible_to_input(v) {
+                    return;
+                }
+
                 let bounds = obj.bounds();
                 if let Some(&(_, pos)) = event
                     .0
@@ -53,6 +76,11 @@ pub fn interaction_handler<T, W: ui::WidgetChildren<T>>(
                 if !mask.release {
                     return;
                 }
+                let v = obj.visible();
+                if !ignore_vis && invisible_to_input(v) {
+                    return;
+                }
+
                 let bounds = obj.bounds();
                 if let Some(&(_, pos)) = event
                     .0
@@ -69,6 +97,11 @@ pub fn interaction_handler<T, W: ui::WidgetChildren<T>>(
                 if !mask.begin_hover && !mask.end_hover {
                     return;
                 }
+                let v = obj.visible();
+                if !ignore_vis && invisible_to_input(v) {
+                    return;
+                }
+
                 let bounds = obj.bounds();
                 let was_hovered = obj.common().with(|x| x.interaction.hovered);
                 let pos = if let Some(&pos) = event.0.with(|&pos| bounds.contains(pos)) {
@@ -135,6 +168,151 @@ pub fn interaction_forwarder<E: ui::Element<Aux = T>, T: 'static>(
             }
         }
     }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum FocusEvent {
+    Gained,
+    Lost,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum FocusMouseTrigger {
+    Press,
+    Release,
+}
+
+impl Default for FocusMouseTrigger {
+    #[inline]
+    fn default() -> Self {
+        FocusMouseTrigger::Press
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct FocusConfig {
+    pub mouse_trigger: FocusMouseTrigger,
+    /// The event ID of the widget which emits interaction events.
+    /// This should almost always be the same ID as the the widget which is attaching the `focus_handler`.
+    pub interaction_handler: u64,
+}
+
+pub fn focus_handler<T, W: ui::WidgetChildren<T>>(
+    aux: &mut ui::Aux<T>,
+    callback: impl Fn(&mut W, &mut ui::Aux<T>, FocusEvent) + Copy + 'static,
+    focus_config: FocusConfig,
+) -> ui::Listener<W, ui::Aux<T>> {
+    aux.listen()
+        .and_on(
+            focus_config.interaction_handler,
+            move |obj: &mut W, aux: &mut ui::Aux<T>, _: &PressEvent| {
+                if focus_config.mouse_trigger == FocusMouseTrigger::Press {
+                    aux.grab_focus(obj.common().clone());
+                }
+            },
+        )
+        .and_on(
+            focus_config.interaction_handler,
+            move |obj: &mut W, aux: &mut ui::Aux<T>, _: &ReleaseEvent| {
+                if focus_config.mouse_trigger == FocusMouseTrigger::Release {
+                    aux.grab_focus(obj.common().clone());
+                }
+            },
+        )
+        .and_on(
+            aux.id,
+            move |obj: &mut W, aux: &mut ui::Aux<T>, evt: &ui::FocusChangedEvent| {
+                if evt
+                    .old_focus
+                    .as_ref()
+                    .map(|x| x == obj.common())
+                    .unwrap_or(false)
+                {
+                    callback(obj, aux, FocusEvent::Lost);
+                } else if evt
+                    .new_focus
+                    .as_ref()
+                    .map(|x| x == obj.common())
+                    .unwrap_or(false)
+                {
+                    callback(obj, aux, FocusEvent::Gained);
+                }
+            },
+        )
+}
+
+pub fn focus_forwarder<E: ui::Element<Aux = T>, T: 'static>(
+) -> impl Fn(&mut E, &mut ui::Aux<T>, FocusEvent) + Copy {
+    move |obj, aux, event| match event {
+        FocusEvent::Gained => {
+            obj.emit(aux, FocusGainedEvent);
+        }
+        FocusEvent::Lost => {
+            obj.emit(aux, FocusLostEvent);
+        }
+    }
+}
+
+pub enum KeyboardEvent {
+    KeyPress(ui::KeyInput),
+    KeyRelease(ui::KeyInput),
+    Text(char),
+}
+
+pub fn keyboard_handler<T, W: ui::WidgetChildren<T>>(
+    aux: &mut ui::Aux<T>,
+    callback: impl Fn(&mut W, &mut ui::Aux<T>, KeyboardEvent) + Copy + 'static,
+) -> ui::Listener<W, ui::Aux<T>> {
+    aux.listen()
+        .and_on(
+            aux.id,
+            move |obj: &mut W, aux: &mut ui::Aux<T>, event: &ui::KeyPressEvent| {
+                if invisible_to_input(obj.visible()) {
+                    return;
+                }
+
+                if let Some(e) = event.0.with(|_| aux.has_focus(obj.common())) {
+                    callback(obj, aux, KeyboardEvent::KeyPress(*e));
+                }
+            },
+        )
+        .and_on(
+            aux.id,
+            move |obj: &mut W, aux: &mut ui::Aux<T>, event: &ui::KeyReleaseEvent| {
+                if invisible_to_input(obj.visible()) {
+                    return;
+                }
+
+                if let Some(e) = event.0.with(|_| aux.has_focus(obj.common())) {
+                    callback(obj, aux, KeyboardEvent::KeyRelease(*e));
+                }
+            },
+        )
+        .and_on(
+            aux.id,
+            move |obj: &mut W, aux: &mut ui::Aux<T>, event: &ui::TextEvent| {
+                if invisible_to_input(obj.visible()) {
+                    return;
+                }
+
+                if let Some(e) = event.0.with(|_| aux.has_focus(obj.common())) {
+                    callback(obj, aux, KeyboardEvent::Text(*e));
+                }
+            },
+        )
+}
+
+pub fn keyboard_forwarder<E: ui::Element<Aux = T>, T: 'static>(
+) -> impl Fn(&mut E, &mut ui::Aux<T>, KeyboardEvent) + Copy {
+    move |obj, aux, event| match event {
+        KeyboardEvent::KeyPress(x) => obj.emit(aux, KeyPressEvent(x)),
+        KeyboardEvent::KeyRelease(x) => obj.emit(aux, KeyReleaseEvent(x)),
+        KeyboardEvent::Text(x) => obj.emit(aux, TextEvent(x)),
+    }
+}
+
+pub fn invisible_to_input(v: ui::Visibility) -> bool {
+    v == ui::Visibility::NoSelf || v == ui::Visibility::Invisible || v == ui::Visibility::None
 }
 
 /// Convenience builder-like utility around the label widget.
---
+++ src/kit/text_box.rs
@@ -0,0 +1,232 @@
+use {
+    crate::{kit, prelude::*, theme, ui},
+    reclutch::display as gfx,
+};
+
+/// Widget which can accept various forms of string-based user input.
+///
+/// This widget shouldn't be used on its own. It is deliberately rendered as only the text and cursor.
+/// Thus, it makes it possible to create more versatile text input widgets on top of this.
+///
+/// If you want a simple text input widget, instead use [`TextInput`](crate::kit::TextInput).
+pub struct TextBox<T: 'static> {
+    text_label: kit::Label<T>,
+    text: String,
+    placeholder: String,
+    wrap: bool,
+    censor: Option<Box<dyn FnMut(&str) -> String>>,
+    multi_line: bool,
+    cursor: usize,
+
+    painter: theme::Painter<Self, T>,
+    common: ui::CommonRef,
+    listeners: ui::ListenerList<Self, ui::Aux<T>>,
+}
+
+impl<T: 'static> TextBox<T> {
+    pub fn new(parent: ui::CommonRef, aux: &mut ui::Aux<T>) -> Self {
+        let common = ui::CommonRef::new(parent);
+
+        let interaction_listener =
+            kit::interaction_handler(aux, kit::interaction_forwarder(None), None, None);
+
+        let focus_listener = kit::focus_handler(
+            aux,
+            kit::focus_forwarder(),
+            kit::FocusConfig {
+                mouse_trigger: Default::default(),
+                interaction_handler: common.with(|x| x.id()),
+            },
+        );
+
+        let keyboard_listener = kit::keyboard_handler(aux, |obj: &mut Self, aux, event| {
+            let mut text = obj.text().to_string();
+            match event {
+                kit::KeyboardEvent::Text(c) => {
+                    text.insert(obj.cursor, c);
+                    obj.cursor += 1;
+                }
+                kit::KeyboardEvent::KeyPress(key) => match key {
+                    ui::KeyInput::Back if obj.cursor > 0 => {
+                        obj.cursor -= 1;
+                        text.remove(obj.cursor);
+                    }
+                    ui::KeyInput::Left if obj.cursor > 0 => {
+                        obj.cursor -= 1;
+                    }
+                    ui::KeyInput::Right if obj.cursor < text.len() => {
+                        obj.cursor += 1;
+                    }
+                    _ => {}
+                },
+                _ => {}
+            }
+            obj.set_text(text);
+
+            kit::keyboard_forwarder()(obj, aux, event);
+        });
+
+        TextBox {
+            text_label: kit::Label::new(common.clone(), aux),
+            text: Default::default(),
+            placeholder: Default::default(),
+            wrap: false,
+            censor: None,
+            multi_line: false,
+            cursor: 0,
+
+            painter: theme::get_painter(aux.theme.as_ref(), theme::painters::TEXT_BOX),
+            common,
+            listeners: ui::ListenerList::new(vec![
+                interaction_listener,
+                focus_listener,
+                keyboard_listener,
+            ]),
+        }
+    }
+
+    pub fn set_text(&mut self, text: impl Into<String>) {
+        self.text = text.into();
+        self.cursor = self.cursor.min(self.text.len());
+        self.update_label();
+    }
+
+    #[inline]
+    pub fn text(&self) -> &str {
+        &self.text
+    }
+
+    pub fn set_placeholder(&mut self, placeholder: impl Into<String>) {
+        self.placeholder = placeholder.into();
+        self.update_label();
+    }
+
+    #[inline]
+    pub fn placeholder(&self) -> &str {
+        &self.placeholder
+    }
+
+    /// Changes whether text which overflows the width can be rendered on a separate line.
+    ///
+    /// Note: This is not the same as physical newlines; it is merely a modification in rendering.
+    pub fn set_wrap(&mut self, wrap: bool) {
+        self.wrap = wrap;
+        self.update_label();
+    }
+
+    #[inline]
+    pub fn wrap(&self) -> bool {
+        self.wrap
+    }
+
+    /// Changes the (optional) censor function.
+    /// The censor function can take a string slice and return a new string with arbitrary censorship applied (e.g. password field).
+    ///
+    /// For a premade censor function for passwords, see [`password_censor`](password_censor).
+    pub fn set_censor(&mut self, censor: impl FnMut(&str) -> String + 'static) {
+        self.censor = Some(Box::new(censor));
+        self.update_label();
+    }
+
+    /// Resets the censor function; no censor function will be applied.
+    pub fn reset_censor(&mut self) {
+        self.censor = None;
+        self.update_label();
+    }
+
+    /// Returns the censor function, if any.
+    #[inline]
+    pub fn censor(&self) -> Option<&dyn FnMut(&str) -> String> {
+        self.censor.as_ref().map(|x| x.as_ref())
+    }
+
+    /// Changes the multi-line ability of this textbox.
+    ///
+    /// This differs from the wrapping mode ([`set_wrap`](TextBox::set_wrap)), in that wrapping is
+    /// the ability to overflow text without clipping, whereas multi-line is the ability to
+    /// receive newline inputs to create physical newlines (e.g. user pressing enter/return key).
+    pub fn set_multi_line(&mut self, multi_line: bool) {
+        self.multi_line = multi_line;
+        self.update_label();
+    }
+
+    /// Returns the state of the multi-line ability of this textbox.
+    #[inline]
+    pub fn multi_line(&self) -> bool {
+        self.multi_line
+    }
+
+    #[inline]
+    pub fn set_cursor(&mut self, cursor: usize) {
+        self.cursor = cursor;
+    }
+
+    #[inline]
+    pub fn cursor(&self) -> usize {
+        self.cursor
+    }
+
+    fn update_label(&mut self) {
+        let mut text = if self.text.is_empty() {
+            self.placeholder.clone()
+        } else {
+            self.text.clone()
+        };
+
+        if let Some(censor) = &mut self.censor {
+            text = censor(&text);
+        }
+
+        if !self.multi_line {
+            text = text.replace(&['\n', '\r'][..], "");
+        }
+
+        self.text_label.set_text(text);
+        self.text_label.set_max_width(if self.wrap {
+            Some(self.bounds().size.width)
+        } else {
+            None
+        });
+    }
+}
+
+impl<T: 'static> ui::Element for TextBox<T> {
+    type Aux = T;
+
+    #[inline]
+    fn common(&self) -> &ui::CommonRef {
+        &self.common
+    }
+
+    fn update(&mut self, aux: &mut ui::Aux<T>) {
+        self.text_label
+            .set_color(aux.theme.color(if self.text.is_empty() {
+                theme::colors::WEAK_FOREGROUND
+            } else {
+                theme::colors::FOREGROUND
+            }));
+
+        ui::propagate_repaint(self);
+        ui::dispatch_list(self, aux, |x| &mut x.listeners);
+    }
+
+    fn draw(&mut self, display: &mut dyn gfx::GraphicsDisplay, aux: &mut ui::Aux<T>) {
+        ui::draw(
+            self,
+            |o, aux| theme::paint(o, |o| &mut o.painter, aux),
+            display,
+            aux,
+            None,
+        );
+    }
+}
+
+impl<T: 'static> ui::WidgetChildren<T> for TextBox<T> {
+    crate::children![for <T>; text_label];
+}
+
+/// Censor function for [`TextBox`](TextBox), appropriate for password fields.
+#[inline]
+pub fn password_censor(s: &str) -> String {
+    "•".repeat(s.len())
+}
